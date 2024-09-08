fb776ae3885663fe21333838e34f00178e4dc1df
Maksym Yarema <macsym2@gmail.com>
2024-09-08 14:51:12 +0300

    feat: add tableInit.sql

---
+++ tableInit.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    age INT
+);
+
+-- CREATE TABLE users ( id BIGSERIAL PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), age INT );
+
