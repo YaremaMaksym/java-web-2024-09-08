e00f3cddd9015e8fa02b95411d56759da7b77cbf
Maksym Yarema <macsym2@gmail.com>
2024-09-08 14:49:37 +0300

    fix HelloServlet

--- src/main/java/org/yaremax/javaweb20240908/servlet/HelloServlet.java
+++ src/main/java/org/yaremax/javaweb20240908/servlet/HelloServlet.java
@@ -1,16 +1,16 @@
-package org.yaremax.javaweb20240908;
+package org.yaremax.javaweb20240908.servlet;
 
 import java.io.*;
 
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "HelloServlet", value = "/hello")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Helloooooooooooo World!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
