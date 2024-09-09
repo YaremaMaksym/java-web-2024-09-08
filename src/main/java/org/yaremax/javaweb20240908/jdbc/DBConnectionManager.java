0031b70d3714eb4becd37a6fdf5dc1b666f5fef9
Maksym Yarema <macsym2@gmail.com>
2024-09-09 13:35:02 +0300

    ref: set remote db address

--- src/main/java/org/yaremax/javaweb20240908/jdbc/DBConnectionManager.java
+++ src/main/java/org/yaremax/javaweb20240908/jdbc/DBConnectionManager.java
@@ -1,12 +1,14 @@
 package org.yaremax.javaweb20240908.jdbc;
 
+import jakarta.persistence.EntityManager;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnectionManager {
     private static DBConnectionManager instance;
-    private static final String URL = "jdbc:postgresql://localhost:5432/ag_db";
+    private static final String URL = "jdbc:postgresql://167.99.244.128:5432/ag_db";
     private static final String USER = "postgres";
     private static final String PASSWORD = "pass";
 
